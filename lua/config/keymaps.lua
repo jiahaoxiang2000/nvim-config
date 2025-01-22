1d5e2abfce6bf331c7676ed7796bac974e53e08e
isomo <jiahaoxiang2000@gmail.com>
2025-01-22 14:02:38 +0800

    how to use the keymap configure

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- map({ "n", "x" }, "k", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true }) -- like this to change overwiew the keyboard map
