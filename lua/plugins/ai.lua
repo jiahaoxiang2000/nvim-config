84b9972a0745db63f753fc499b958db5a9d8bd70
isomo <jiahaoxiang2000@gmail.com>
2025-01-23 21:06:48 +0800

    use the lazyextra to control the plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,5 +1,6 @@
 -- this file we will to config the ai plugin
 -- TODO: interget the copliot
+-- can use the `:LazyExtras` to enable the copilot, which is built-in in the lazyvim extras package
 
 return {
   {},
