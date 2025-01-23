91f7fd8e794906d47f9ed730e9bdf0d59e7f68eb
isomo <jiahaoxiang2000@gmail.com>
2025-01-23 22:45:23 +0800

    configure the blik cmp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+
+-- Define `config` to set up custom auto commands this is for hide the suggestion of the coplit. by https://cmp.saghen.dev/recipes.html#hide-copilot-on-suggestion
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    require("copilot.suggestion").dismiss()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
