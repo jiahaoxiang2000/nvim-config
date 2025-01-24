cc100839015c7153f581086d4ce1a9a46a20c180
isomo <jiahaoxiang2000@gmail.com>
2025-01-24 12:57:38 +0800

    change the colorscheme for ide

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- i like the dark color scheme mroe than the light one
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
