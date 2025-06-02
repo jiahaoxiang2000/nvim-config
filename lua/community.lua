c850d0bca32edafca9d1a0fa60a32e975b5c43c0
isomo <jiahaoxiang2000@gmail.com>
2025-06-02 11:10:18 +0800

    add the new config

---
+++ lua/community.lua
@@ -0,0 +1,26 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.git.gitgraph-nvim" },
+
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.motion.marks-nvim" },
+
+  { import = "astrocommunity.project.projectmgr-nvim" },
+  { import = "astrocommunity.editing-support.wildfire-nvim" },
+
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.utility.noice-nvim" },
+}
---
+++ lua/plugins/astrolsp.lua
@@ -0,0 +1,11 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  "AstroNvim/astrolsp",
+  ---@type AstroLSPOpts
+  opts = {
+    formatting = {
+      format_on_save = false, -- enable or disable automatic formatting on save
+    },
+  },
+}
