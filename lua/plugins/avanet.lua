89c8fc37e50735933f0a1f5752399e5345eff948
isomo <jiahaoxiang2000@gmail.com>
2025-06-03 18:07:54 +0800

    ai: add the planing model and the auto apply diff

--- lua/plugins/avanet.lua
+++ lua/plugins/avanet.lua
@@ -31,10 +31,11 @@ return {
       auto_suggestions = true, -- Experimental stage
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
-      auto_apply_diff_after_generation = false,
+      auto_apply_diff_after_generation = true,
       support_paste_from_clipboard = true,
       minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
       enable_token_counting = true, -- Whether to enable token counting. Default to true.
+      enable_cursor_planning_mode = true, -- enable cursor planning mode!
     },
     mappings = {
       ask = prefix .. "<CR>",
