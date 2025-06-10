f8709f0439b7679693c96d066388e69996532ea0
isomo <jiahaoxiang2000@gmail.com>
2025-06-10 21:48:34 +0800

    feat: disable VectorCode plugin dependency
    Comment out VectorCode plugin configuration in codecompanion.lua

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -13,12 +13,12 @@ return {
       end,
     },
     { "echasnovski/mini.diff", opts = {} },
-    {
-      "Davidyz/VectorCode",
-      version = "*", -- optional, depending on whether you're on nightly or release
-      -- build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
+    -- {
+    --   "Davidyz/VectorCode",
+    --   version = "*", -- optional, depending on whether you're on nightly or release
+    --   -- build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
+    --   dependencies = { "nvim-lua/plenary.nvim" },
+    -- },
     {
       "AstroNvim/astrocore",
       ---@param opts AstroCoreOpts
