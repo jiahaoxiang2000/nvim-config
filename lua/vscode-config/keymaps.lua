b01dfc54fb9e1c6c2434e564fa6f9f25a455e44f
isomo <jiahaoxiang2000@gmail.com>
2025-07-17 22:11:24 +0800

    ✨ feat: add problem navigation keybindings for diagnostics

--- lua/vscode-config/keymaps.lua
+++ lua/vscode-config/keymaps.lua
@@ -143,6 +143,15 @@ vim.keymap.set('n', '[c', function()
     vscode.action('workbench.action.editor.previousChange')
 end, { desc = 'Go to previous change in compare/diff editor' })
 
+-- Problem navigation
+vim.keymap.set('n', ']d', function()
+    vscode.action('editor.action.marker.next')
+end, { desc = 'Go to next diagnostic' })
+
+vim.keymap.set('n', '[d', function()
+    vscode.action('editor.action.marker.prev')
+end, { desc = 'Go to previous diagnostic' })
+
 -- Show current file information using JavaScript evaluation
 vim.keymap.set('n', '<leader>fi', function()
     local file_info = vscode.eval([[
