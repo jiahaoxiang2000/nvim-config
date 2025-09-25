271cfdf8c2d751acf683b610c7b1032d862fcc5c
isomo <jiahaoxiang2000@gmail.com>
2025-09-25 16:10:25 +0800

    chore: for hyprland copyboard

--- lua/vscode-config/config/options.lua
+++ lua/vscode-config/config/options.lua
@@ -4,5 +4,20 @@ vim.g.maplocalleader = "\\"
 
 -- VSCode-specific settings
 vim.opt.clipboard = "unnamedplus"
+
+-- Wayland clipboard provider for Hyprland
+vim.g.clipboard = {
+  name = 'wl-clipboard',
+  copy = {
+    ['+'] = 'wl-copy',
+    ['*'] = 'wl-copy',
+  },
+  paste = {
+    ['+'] = 'wl-paste --no-newline',
+    ['*'] = 'wl-paste --no-newline',
+  },
+  cache_enabled = 0,
+}
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = false
